fb05518d638e4e144c61fad41c7df4afcc13287d
Wentuziak <gabrielurbanski@wp.pl>
2024-04-16 23:13:15 +0200

    Camera movement

--- Camera.gd
+++ Camera.gd
@@ -1,21 +1,18 @@
 extends SpringArm3D
 
-# Sensitivity for mouse movement
-var sensitivity = 0.1
-
-# Variables to store previous mouse position
+var sensitivity = 0.05
 var prev_mouse_position = Vector2.ZERO
 
 func _ready():
-	# Hide the mouse cursor
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _input(event):
+	
 	if event is InputEventMouseMotion:
-		# Capture mouse movement
+		
 		var mouse_delta = event.relative
-		# Rotate the camera based on mouse movement
 		rotate_y(mouse_delta.x * sensitivity)
-		rotate_x(mouse_delta.y * sensitivity)
+		
+
 
 
